4994ab6ed0af82aee271c5f30b9cc48413cee5b7
Markus Hiller <markus.hiller@student.unimelb.edu.au>
2025-03-04 17:12:58 +1100

    Added bash scripts as examples how to run experiments on 1 or multiple GPUs -- Paths need to be adapted by end-user.

---
+++ experiment_scripts/train_bixt_ti_l64_p16/bixt_ep800_4gpus.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+#echo "Starting up environment..."
+#source /<path_to_miniconda3>/bin/activate bixt_env
+#echo "... done!"
+#
+cd ../..
+#
+#
+export CUDA_DEVICE_ORDER='PCI_BUS_ID'
+export CUDA_VISIBLE_DEVICES='0,1,2,3'
+#
+export WANDB_API_KEY="<INSERT_YOUR_KEY_HERE>"  # and activate using argument, if tracking via wandb is desired
+#
+echo "Attempting to set up training..."
+export MASTER_ADDR=127.0.0.1
+export MASTER_PORT=12347
+export WORLD_SIZE=4
+echo "Using Master_Addr $MASTER_ADDR:$MASTER_PORT to synchronise, and a world size of $WORLD_SIZE."
+#
+#
+echo "Setting path to dataset (ImageNet), as well as output directory for checkpoints"
+export DATAPATH='/mnt/datasets/ILSVRC2012'
+export OUTPUT_DIR='/mnt/<your_checkpoint_path>/BiXT_checkpoints'
+#
+echo "Start training the default tiny BiXT model with 64 latents, using the default hyperparameters and total batch size of 1024..."  #
+torchrun --nproc_per_node=4 --rdzv-endpoint=$MASTER_ADDR:$MASTER_PORT train_BiXT.py --torchcompile inductor --model bixt_ti_l64_p16 --seed 42 --lr 2.5e-3 --sa_drop_path 0.00 --ca_drop_path 0.00 --workers 6 --warmup_lr 1e-6 --data_path $DATAPATH --batch_size_per_gpu 256 --epochs 800 --weight_decay 0.05 --sched cosine --reprob 0.0 --smoothing 0.0 --warmup_epochs 5 --drop 0.0 --opt lambc --mixup .8 --cutmix 1.0 --bce_loss --color_jitter 0.3 --three_augment --output_dir $OUTPUT_DIR
+echo "Finished training. Cheers!"
+
---
+++ experiment_scripts/train_bixt_ti_l64_p16_ft384/bixt_ft384_ep30_2gpus.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+#echo "Starting up environment..."
+#source /<path_to_miniconda3>/bin/activate bixt_env
+#echo "... done!"
+#
+cd ../..
+#
+#
+export CUDA_DEVICE_ORDER='PCI_BUS_ID'
+export CUDA_VISIBLE_DEVICES='0,1'
+#
+export WANDB_API_KEY="<INSERT_YOUR_KEY_HERE>"  # and activate using argument, if tracking via wandb is desired
+#
+echo "Attempting to set up training..."
+export MASTER_ADDR=127.0.0.1
+export MASTER_PORT=12345
+export WORLD_SIZE=2
+echo "Using Master_Addr $MASTER_ADDR:$MASTER_PORT to synchronise, and a world size of $WORLD_SIZE."
+#
+#
+echo "Setting path to dataset (ImageNet), as well as output directory for checkpoints"
+export DATAPATH='/mnt/datasets/ILSVRC2012'
+export OUTPUT_DIR='/mnt/<your_checkpoint_path>/BiXT_checkpoints/FT'
+#
+echo "Start fine-tuning on 384x384 images, using total batch size of 512..."
+torchrun --nproc_per_node=2 --rdzv-endpoint=$MASTER_ADDR:$MASTER_PORT train_BiXT.py --model bixt_ti_l64_p16_ft384_e800 --input_size 3 384 384 --pretrained --seed 42 --lr 2.5e-5 --sa_drop_path 0.05 --ca_drop_path 0.05 --workers 6 --torchcompile inductor --data_path $DATAPATH --batch_size_per_gpu 256 --epochs 30 --weight_decay 0.05 --sched cosine --reprob 0.0 --smoothing 0.0 --warmup_epochs 0 --drop 0.0 --opt lambc --mixup .8 --cutmix 1.0 --bce_loss --color_jitter 0.3 --three_augment --output_dir $OUTPUT_DIR
+echo "Finished training. Cheers!"
